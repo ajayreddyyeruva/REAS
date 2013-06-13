ab8065b1ba5dfb09badc6129b5b6ac00fd3ea6aa
Sandeep <sandeep.rawat@jasperindia.com>
2013-06-13 13:55:30 +0530

    Second version where a user can't unlock screen in next few minutes

---
+++ screen_lock.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+DISPLAY=:0 vlc --play-and-exit -f /home/user/Downloads/screen_lock.mp4
+
+SCREEN_SAVER_START_TIME=`date +"%s"`
+SCREEN_SAVER_END_TIME=$(($SCREEN_SAVER_START_TIME+60))
+
+DISPLAY=:0 gnome-screensaver-command -l
+
+SCREEN_SAVER_CURRENT_TIME=`date +"%s"`
+while [ $SCREEN_SAVER_CURRENT_TIME -le $SCREEN_SAVER_END_TIME ]
+do
+
+	SCREEN_STATUS=`DISPLAY=:0 gnome-screensaver-command -q`
+	echo "Screen status $SCREEN_STATUS"
+	if [ "The screensaver is inactive" = "${SCREEN_STATUS}" ]
+	then
+		echo "As the screen lock time is not over locking it again"
+		DISPLAY=:0 gnome-screensaver-command -l
+	else
+		echo "Screeen saver is active"
+	fi
+	SCREEN_SAVER_CURRENT_TIME=`date +"%s"`
+	sleep 1
+done
+
